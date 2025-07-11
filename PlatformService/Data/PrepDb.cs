c4a1546e61ce1b4f2a1d5702ab4ba111b8640f8a
Edvinas09 <edvinas.bagdonas10@gmail.com>
2025-07-11 21:05:31 +0300

    Configured sql db

--- PlatformService/Data/PrepDb.cs
+++ PlatformService/Data/PrepDb.cs
@@ -3,22 +3,35 @@ using Microsoft.Extensions.DependencyInjection;
 using PlatformService.Models;
 using System;
 using System.Linq;
-using PlatformService.Data; // <-- Add this line
+using PlatformService.Data;
+using Microsoft.EntityFrameworkCore; // <-- Add this line
 
 namespace PlatformService.Data
 {
     public static class PrepDb
     {
-        public static void PrepPopulation(IApplicationBuilder app)
+        public static void PrepPopulation(IApplicationBuilder app, bool isProd)
         {
             using (var serviceScope = app.ApplicationServices.CreateScope())
             {
-                SeedData(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>());
+                SeedData(serviceScope.ServiceProvider.GetRequiredService<AppDbContext>(), isProd);
             }
         }
 
-        private static void SeedData(AppDbContext context)
+        private static void SeedData(AppDbContext context, bool isProd)
         {
+            if (isProd)
+            {
+                Console.WriteLine("--> Applying Migrations...");
+                try
+                {
+                    context.Database.Migrate();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"--> Could not run migrations: {ex.Message}");
+                }
+            }
             if (!context.Platforms.Any())
             {
                 Console.WriteLine("--> Seeding Data...");
--- PlatformService/Program.cs
+++ PlatformService/Program.cs
@@ -9,8 +9,21 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<AppDbContext>(options =>
+
+
+if (builder.Environment.IsProduction())
+{
+    Console.WriteLine("--> Using SQL Server Database");
+    builder.Services.AddDbContext<AppDbContext>(options =>
+        options.UseSqlServer(builder.Configuration.GetConnectionString("PlatformsConn")));
+}
+else
+{
+    Console.WriteLine("--> Using InMemory Database");
+    builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseInMemoryDatabase("InMem"));
+}
+
 builder.Services.AddScoped<IPlatformRepo, PlatformRepo>();
 builder.Services.AddHttpClient<ICommandDataClient, HttpCommandDataClient>();
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
@@ -27,5 +40,5 @@ if (app.Environment.IsDevelopment())
 
 //app.UseHttpsRedirection();
 app.MapControllers();
-PrepDb.PrepPopulation(app); // Seed the database with initial data
+PrepDb.PrepPopulation(app, builder.Environment.IsProduction()); // Seed the database with initial data
 app.Run();
